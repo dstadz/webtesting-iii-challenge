ecccfd87429cd647b93eca62f4b4846e779ad34f
Daniel <danstad2012@gmail.com>
2019-11-11 14:18:26 -0800

    initialize

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,10 @@
 // Test away!
+// Test away
+import React from 'react'
+import { render } from '@testing-library/react';
+import Controls from './Controls'
+
+
+test("Dashoard renders", () => {
+  render(<Controls/>)
+})
--- src/dashboard/Dashboard.test.js
+++ src/dashboard/Dashboard.test.js
@@ -1,10 +1,9 @@
-// Test away!
 // Test away
 import React from 'react'
 import { render } from '@testing-library/react';
-import Display from './Display'
+import Dashboard from './Dashboard'
 
 
 test("Dashoard renders", () => {
-  render(<Display/>)
+  render(<Dashboard/>)
 })
